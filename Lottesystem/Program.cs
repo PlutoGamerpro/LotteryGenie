b56cb8156fa973c7c670703e3c34cb068f2d61ec
Gustav <114870315+PlutoGamerpro@users.noreply.github.com>
2024-10-14 08:32:00 +0200

    Add files via upload

---
+++ Lottesystem/Program.cs
@@ -0,0 +1,110 @@
+﻿
+
+
+namespace Lottesystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Random SpecialNumber = new Random();
+
+
+            Console.WriteLine("Lotte Kupon");
+            List<int> list = new List<int>();
+            int pickednumber;
+            int specialnumber;
+
+            Lotte(list);
+
+
+
+            void Lotte(List<int> lst)
+            {
+                int currentlyLine = 1;
+
+                for (int j = 0; currentlyLine < 7; j++)
+                {
+                    Console.WriteLine("");
+
+                    for (int i = 0; i < 7; i++) // forloops geneart 7 random numbers
+                    {
+                        pickednumber = rnd.Next(1, 36);
+                        specialnumber = rnd.Next(1, 36);
+
+                        while (lst.Contains(pickednumber))
+                        {
+                            pickednumber = rnd.Next(1, 36);
+
+
+                        }
+                        lst.Add(pickednumber);
+
+                        if (pickednumber == specialnumber)
+                        {
+                            
+                            Console.ForegroundColor = ConsoleColor.Green; Console.Write(pickednumber + " ");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            int liste = lst[i];
+                            Console.Write(pickednumber + " ");
+                        }
+
+
+
+
+                        if (i == 7)
+                        {
+                            for (int l = 0; l < lst.Count; l++)
+                            {
+                                lst.Remove(l);
+                            }
+                            i = 0;
+                            currentlyLine++;
+                        }
+                    }
+                  
+                }
+
+            }
+
+
+
+        }
+    }
+}
+
+/*
+void Lotte(List<int> lst)
+{
+
+        for (int i = 0; i < 7; i++)
+        {
+            pickednumber = rnd.Next(1, 36);
+
+            while (lst.Contains(pickednumber))
+            {
+                pickednumber = rnd.Next(1, 36);
+
+            }
+            lst.Add(pickednumber);
+
+            int liste = lst[i];
+            Console.Write(pickednumber + " ");
+        }
+
+    Console.WriteLine("");
+}
+
+
+
+
+
+
+}
+}
+}
+*/
